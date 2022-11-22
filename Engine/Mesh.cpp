85f70a83f0708fdaa453d8e5028bd13c402e8cb4
HanSanghyun <hshf123@naver.com>
2022-11-22 15:41:56 +0900

    Index Buffer

--- Engine/Mesh.cpp
+++ Engine/Mesh.cpp
@@ -6,10 +6,16 @@
 //		Mesh
 // ---------------
 
-void Mesh::Init(vector<Vertex>& vec)
+void Mesh::Init(const vector<Vertex>& vertexBuffer, const vector<uint32>& indexBuffer)
+{
+	CreateVertexBuffer(vertexBuffer);
+	CreateIndexBuffer(indexBuffer);
+}
+
+void Mesh::CreateVertexBuffer(const vector<Vertex>& buffer)
 {
 	HRESULT hr;
-	_vertexCount = static_cast<uint32>(vec.size());
+	_vertexCount = static_cast<uint32>(buffer.size());
 	uint32 bufferSize = _vertexCount * sizeof(Vertex);
 
 	D3D11_BUFFER_DESC bd = {};
@@ -19,9 +25,26 @@ void Mesh::Init(vector<Vertex>& vec)
 	bd.CPUAccessFlags = 0;
 
 	D3D11_SUBRESOURCE_DATA InitData = {};
-	InitData.pSysMem = &vec[0];
+	InitData.pSysMem = &buffer[0];
 	hr = DEVICE->CreateBuffer(&bd, &InitData, &_vertexBuffer);
 	CHECK_FAIL(hr, L"Failed Create Vertex Buffer");
+}
+
+void Mesh::CreateIndexBuffer(const vector<uint32>& buffer)
+{
+	HRESULT hr;
+	_indexCount = static_cast<uint32>(buffer.size());
+	uint32 bufferSize = _indexCount * sizeof(uint32);
+
+	D3D11_BUFFER_DESC bd = {};
+	bd.Usage = D3D11_USAGE_DEFAULT;
+	bd.ByteWidth = bufferSize;
+	bd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	bd.CPUAccessFlags = 0;
+	D3D11_SUBRESOURCE_DATA InitData = {};
+	InitData.pSysMem = &buffer[0];
+	hr = DEVICE->CreateBuffer(&bd, &InitData, &_indexBuffer);
+	CHECK_FAIL(hr, L"Failed Create Index Buffer");
 }
 
 void Mesh::Render()
@@ -30,11 +53,13 @@ void Mesh::Render()
 	uint32 stride = sizeof(Vertex);
 	uint32 offset = 0;
 	DEVICECTX->IASetVertexBuffers(0, 1, &_vertexBuffer, &stride, &offset);
+	// Set index buffer
+	DEVICECTX->IASetIndexBuffer(_indexBuffer, DXGI_FORMAT_R32_UINT, 0);
 	// Set primitive topology
 	DEVICECTX->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
 	GEngine->GetCB()->PushData(0, &_transform);
 	GEngine->GetCB()->PushData(1, &_transform);
 
-	DEVICECTX->Draw(_vertexCount, 0);
+	DEVICECTX->DrawIndexed(_indexCount, 0, 0);
 }
--- Engine/Mesh.h
+++ Engine/Mesh.h
@@ -7,14 +7,20 @@
 class Mesh
 {
 public:
-	void Init(vector<Vertex>& vec);
+	void Init(const vector<Vertex>& vertexBuffer, const vector<uint32>& indexBuffer);
 	void Render();
 
 	void SetTransform(const Transform& transform) { _transform = transform; }
 
 private:
+	void CreateVertexBuffer(const vector<Vertex>& buffer);
+	void CreateIndexBuffer(const vector<uint32>& buffer);
+
+private:
 	ID3D11Buffer* _vertexBuffer = nullptr;
+	ID3D11Buffer* _indexBuffer = nullptr;
 	uint32 _vertexCount = 0;
+	uint32 _indexCount = 0;
 
 	Transform _transform = {};
 };
